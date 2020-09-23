bd0e2bc5268251de87893169440a5313726e6676
Walid nurudin <walidnurudin47@gmail.com>
2020-09-24 00:50:08 +0700

    data dummy

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charitis: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: '',
+            pledged: 0,
+            target: 100000,
+            date_end: +new Date('10 August 2020'),
+            profile_photo: '',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+        }
+    ]
+}

7940efbbc35f672c7338606916d87273fa1461f8
Walid nurudin <walidnurudin47@gmail.com>
2020-09-25 20:48:51 +0700

    add template

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,17 +1,15 @@
 <script>
+  import Header from "../components/Header.svelte";
+  import Welcome from "../components/Welcome.svelte";
   import CharityList from "../components/CharityList.svelte";
+  import Promo from "../components/Promo.svelte";
+  import Footer from "../components/Footer.svelte";
+
   import { charities } from "../data/charities";
-  let title = "Charity";
 </script>
 
-<style>
-  h1 {
-    font-size: 3em;
-    color: red;
-  }
-</style>
-
-<div>
-  <h1>{title}</h1>
-  <CharityList {charities} />
-</div>
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
